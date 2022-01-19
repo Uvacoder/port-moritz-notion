42a76adc65dae86bc84d41902945e0bd4fdc0cbe
mrzmyr <moritz@devmood.com>
2022-01-19 13:58:54 +0100

    chores(reading): add format to REST call and function name

--- pages/api/reading.js
+++ pages/api/reading.js
@@ -1,5 +1,5 @@
-export default function (req, res) {
-  fetch('https://oku.club/api/collections/user/jadann/reading')
+export default function reading(req, res) {
+  fetch('https://oku.club/api/collections/user/jadann/reading?format=json')
   .then(r => r.json())
   .then(data => {
     res.json(data);
